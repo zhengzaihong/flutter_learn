892b20f74b2af5f5d57bcdc08ea855f9dc8db656
zhengzaihong <1096877329@qq.com>
2019-08-15 18:10:28 +0800

    第三章讲解

--- lib/main.dart
+++ lib/main.dart
@@ -1,139 +1,161 @@
     import 'package:flutter/material.dart';
 
-    import 'res/TestData.dart';
-
     void main() => runApp(MyApp());
-
-    bool isGrid = false;
 
     class MyApp extends StatelessWidget {
       @override
       Widget build(BuildContext context) {
         return new MaterialApp(
-          home: new ViewStatefulWidget(),
+          home: new Scaffold(
+              appBar: AppBar(
+                title: Text("呆萌"),
+              ),
+              body: ViewLayout()),
         );
       }
     }
 
-    //自定义Widget 实现有状态StatefulWidget
-    class ViewStatefulWidget extends StatefulWidget {
+    class ViewLayout extends StatelessWidget {
       @override
-      State<StatefulWidget> createState() {
-        return HomeContent();
+      Widget build(BuildContext context) {
+        return ListView(
+          children: <Widget>[
+            Text(
+              "这是Paddiing的简单应用",
+              style: TextStyle(fontSize: 18, color: Colors.deepOrangeAccent),
+            ),
+
+            SizedBox(
+              height: 10,
+            ),
+            //用容器包装下 方便观察
+            Container(
+              color: Colors.deepPurple,
+              height: 60,
+              child: Padding(
+                //可以通过fromLTRB分别设置左上右下的内距，也可以用all统一设置
+                padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: Image.network(
+                    'http://qukufile2.qianqian.com/data2/pic/246708144/246708144.jpg',
+                    fit: BoxFit.fitHeight),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "这是Row应用(横向布局)",
+              style: TextStyle(fontSize: 18, color: Colors.deepOrangeAccent),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              //spaceEvenly 主轴的排列方式最为常见
+              crossAxisAlignment: CrossAxisAlignment.start,
+              //用的比较少
+              children: <Widget>[
+                IconWidget(Icons.search, color: Colors.blue),
+                IconWidget(Icons.home, color: Colors.orange),
+                IconWidget(Icons.select_all, color: Colors.red),
+              ],
+            ),
+
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              "这是Column应用(垂直布局)",
+              style: TextStyle(fontSize: 18, color: Colors.deepOrangeAccent),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //spaceEvenly 主轴的排列方式最为常见
+              crossAxisAlignment: CrossAxisAlignment.center,
+              //用的比较少
+              children: <Widget>[
+                IconWidget(Icons.search, color: Colors.blue),
+                SizedBox(
+                  height: 5,
+                ),
+                IconWidget(Icons.home, color: Colors.orange),
+                SizedBox(
+                  height: 5,
+                ),
+                IconWidget(Icons.select_all, color: Colors.red),
+              ],
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              "这是Expanded应用权重(1:2:1)",
+              style: TextStyle(fontSize: 18, color: Colors.deepOrangeAccent),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+
+            Row(
+              children: <Widget>[
+                Expanded(
+                    flex: 1, child: IconWidget(Icons.search, color: Colors.blue)),
+                Expanded(
+                  flex: 2,
+                  child: IconWidget(Icons.home, color: Colors.orange),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: IconWidget(Icons.select_all, color: Colors.red),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "这是Expanded应用权重(1:2)",
+              style: TextStyle(fontSize: 18, color: Colors.deepOrangeAccent),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+
+            Row(
+              children: <Widget>[
+                Expanded(
+                    flex: 1, child: IconWidget(Icons.search, color: Colors.blue)),
+                Expanded(
+                  flex: 2,
+                  child: IconWidget(Icons.home, color: Colors.orange),
+                ),
+              ],
+            ),
+          ],
+        );
       }
     }
 
-    class HomeContent extends State<ViewStatefulWidget> {
-      void changeView() {
-        // 通过 setState() 更新数据,组件树自动刷新
-        setState(() {
-          if (isGrid) {
-            isGrid = false;
-          } else {
-            isGrid = true;
-          }
-        });
-      }
+    //封装一个简单的Icon 方便调度
+    class IconWidget extends StatelessWidget {
+      double size = 32.0;
+      Color color = Colors.red;
+      IconData icon;
 
-      List<Widget> _getListData() {
-        var tempList = musicData.map((value) {
-          return Container(
-            child: Column(
-              children: <Widget>[
-                SizedBox(height: 10),
-                Image.network(value["url"],
-                    width: 110, height: 100, fit: BoxFit.fitWidth),
-                SizedBox(height: 10),
-                Text(value["author"],
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 20, color: Colors.white)),
-              ],
-            ),
-            //绘制圆角背景
-            decoration: BoxDecoration(
-                //圆角10个单位
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                //背景色
-                color: Colors.deepPurple,
-                //绘制边框
-                border: Border.all(
-                  color: Colors.deepPurple,
-                  width: 1.0,
-                )),
-          );
-        });
-        return tempList.toList();
-      }
-
-      //通过循环添加
-      List<Widget> _getData() {
-        List<Widget> list = new List();
-
-        for (var i = 0; i < musicData.length; i++) {
-          list.add(
-            Column(
-              children: <Widget>[
-                ListTile(
-                  title: Text(musicData[i]["music"]), //设置标题文本内容
-                  leading: Image.network(musicData[i]["url"]), //在文本前显示网络图片
-                  subtitle: Text(musicData[i]["author"]), // 设置二级标题
-                ),
-                //添加一条分割线
-                new Container(
-                  color: Colors.black12,
-                  height: 1,
-                )
-              ],
-            ),
-          );
-        }
-        return list;
-      }
-
-      // 根据状态返回不同的Widget
-      Widget getWidget() {
-        if (isGrid) {
-          return ListView(
-            children: this._getData(),
-          );
-        } else {
-          return GridView.count(
-            //水平子 Widget 之间间距
-            crossAxisSpacing: 10.0,
-            //垂直子 Widget 之间间距
-            mainAxisSpacing: 10.0,
-            padding: EdgeInsets.all(10),
-            //一行的 Widget 数量
-            crossAxisCount: 2,
-            //宽度和高度的比例
-            // childAspectRatio:0.7,
-
-            children: this._getListData(),
-          );
-        }
-      }
+      IconWidget(this.icon, {this.color, this.size});
 
       @override
       Widget build(BuildContext context) {
-        return new Container(
-          child: new Scaffold(
-              appBar: AppBar(
-                title: Text("呆萌"),
-                 backgroundColor: Colors.red, //appBar 背景色
-                 centerTitle: true, //标题居中显示
-                 actions: <Widget>[
-                  IconButton(
-
-                    icon: Icon(Icons.menu),
-                    onPressed: () { //添加按下事件
-                      changeView();
-                    },
-                  )
-                ],
-              ),
-              body: new Container(
-                child: getWidget(),
-              )),
+        return Container(
+          height: 50.0,
+          width: 50.0,
+          color: this.color,
+          child:
+              Center(child: Icon(this.icon, size: this.size, color: Colors.white)),
         );
       }
     }
